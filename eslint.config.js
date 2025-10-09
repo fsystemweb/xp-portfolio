9fad7f0985f85146fc2345c4e17de94fede00eb0
fsystemweb <facu.cappella@gmail.com>
2025-10-09 18:51:38 +0200

    add eslint rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,25 +1,78 @@
+// eslint.config.js
 import js from '@eslint/js';
-import globals from 'globals';
+import react from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
-import reactRefresh from 'eslint-plugin-react-refresh';
+import unicorn from 'eslint-plugin-unicorn';
 import tseslint from 'typescript-eslint';
+import globals from 'globals';
+import { FlatCompat } from '@eslint/eslintrc';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-export default tseslint.config(
-  { ignores: ['dist'] },
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({ baseDirectory: __dirname });
+
+export default [
+  { ignores: ['dist/', 'node_modules/', 'eslint.config.js', 'vite.config.ts', "main.tsx"] },
+  js.configs.recommended,
+  ...tseslint.configs.recommended, 
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended, 'prettier'],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      sourceType: 'module',
+      parser: tseslint.parser,
+      parserOptions: {
+        project: ['./tsconfig.app.json'],
+        tsconfigRootDir: __dirname,
+      },
+      globals: {
+        ...globals.browser,
+        ...globals.es2021,
+        ...globals.node,
+      },
     },
     plugins: {
+      react,
       'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      unicorn,
+    },
+    settings: {
+      react: { version: 'detect' },
     },
     rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+      ],
+      '@typescript-eslint/no-explicit-any': 'warn',
+      'no-console': ['warn', { allow: ['warn', 'error'] }],
+      'unicorn/prevent-abbreviations': 'off',
+      '@typescript-eslint/no-unused-expressions': [
+        'error',
+        {
+          allowShortCircuit: true,
+          allowTernary: true,
+          allowTaggedTemplates: true,
+        },
+      ],
+      'unicorn/filename-case': ['error', { case: 'pascalCase' }],
     },
-  }
-);
+  },
+  {
+    files: ['**/*.ts'],
+    rules: {
+      'unicorn/filename-case': ['error', { case: 'kebabCase' }],
+    },
+  },
+    {
+    files: ['src/main.tsx'],
+    rules: {
+      'unicorn/filename-case': 'off',
+    },
+  },
+  ...compat.extends('plugin:prettier/recommended'),
+];
